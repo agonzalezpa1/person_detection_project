b46e88f441680d173edf5600ea6a4b54155e4fb7
agonzalezpa1 <agonzalezpa@ensc.fr>
2025-11-20 18:18:39 +0100

    Fichier gitignore et update du training pipeline

--- yolo11n_training_pipeline.py
+++ yolo11n_training_pipeline.py
@@ -10,22 +10,37 @@ path_data_yaml = os.path.join(dataset_path, "data.yaml")
 # Sélectionne le modèle YOLO11n
 model = YOLO("yolo11n.pt")
 
-# Entraîne le modèle avec un retour sur le score F1 après chaque epoch
-def log_f1(epoch) :
-    metrics = model.val() # Récupère les métriques du modèle
-    f1_person = metrics.box.f1[0]
-    print(f"Epoch {epoch + 1} : F1 (person) = {f1_person:.4f}")
-
 # Entraîne le modèle avec le dataset sur Kaggle
+total_epochs = 50
+
+print(f"\n------ Epoch 1 /{total_epochs} -------------------------")
+
 model.train(
     data = path_data_yaml,
-    epochs = 50,
+    epochs = 1,
     imgsz = 640,
     batch = 16,
     name = "yolo11n_person_project",
-    device = 0,
-    callcallbacks = [log_f1]
+    device = "cpu", # Utilisant le CPU de mon ordinateur
 )
+
+for epoch in range(2, total_epochs):
+    print(f"\n------ Epoch {epoch}/{total_epochs} -------------------------")
+
+    model.train(
+        data = path_data_yaml,
+        epochs = 1,
+        imgsz = 640,
+        batch = 16,
+        name = "yolo11n_person_project",
+        device = "cpu", # Utilisant le CPU de mon ordinateur
+        resume = True # Pour continuer le training
+    )
+
+    # Retour sur le score F1 après chaque epoch
+    metrics = model.val()
+    f1_person = metrics.box.f1[0]
+    print(f"F1 (person) after epoch {epoch + 1} : {f1_person:.4f}")
 
 # Message de fin
 print("Training completed using the training pipeline.")
