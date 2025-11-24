1580983e97268016dacf33930c410fd682af5e08
agonzalezpa1 <agonzalezpa@ensc.fr>
2025-11-24 20:22:44 +0100

    Mise à jour pipeline inférence et images de test

--- yolo11n_inference_pipeline.py
+++ yolo11n_inference_pipeline.py
@@ -1,3 +1,4 @@
+import os
 from ultralytics import YOLO
 
 # Sélectionne la meilleure version du modèle YOLO11n en fonction de son score F1
@@ -7,6 +8,7 @@ model = YOLO("/runs/detect/yolo11n_person_project/weights/best.pt")
 # Récupère le path de l'image ou vidéo de test
 # Si dans Google Collab : /content/person_detection_project/test_image_2.jpg
 test_path = "/test_image_2.jpg"
+file_name = os.path.splitext(os.path.basename(test_path))[0]
 
 # Affiche les résultats du nombre de personnes détectées
 results = model.predict(conf = 0.05, source = test_path, save = True)
@@ -14,4 +16,4 @@ r = results[0]
 classes = r.boxes.cls.tolist()
 num_persons = classes.count(4)
 
-print("\n\nNumber of persons detected in the image :", num_persons)
+print(f"\n\nNumber of persons detected in the image '{file_name}' :", num_persons)
